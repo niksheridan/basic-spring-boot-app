c3042154c35511d092c1db0d1aee9b54c2cf9632
A Bangser <bangser.a@gmail.com>
2018-01-18 15:51:22 +0000

    Add a docker image test and necessary wait command in docker-maven plugin pom

--- src/test/java/hello/docker_image_tests/ExampleDockerImageTest.java
+++ src/test/java/hello/docker_image_tests/ExampleDockerImageTest.java
@@ -2,14 +2,20 @@ package hello.docker_image_tests;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 public class ExampleDockerImageTest {
+
+    TestRestTemplate restTemplate = new TestRestTemplate();
+
     @Test
     public void shouldDoSomething() {
-        assertThat(true).isEqualTo(true);
+        ResponseEntity<String> helloWorld = restTemplate.getForEntity("http://localhost:8080/", String.class);
+        assertThat(helloWorld.getBody()).isEqualTo("Hello World!");
     }
 }
